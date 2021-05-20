d7965566e10e3476b96b495475299fa168759c2c
p-onoshi <tomoyasu.ono@paiza.co.jp>
2021-05-20 16:31:04 +0900

    fish abbreviations

---
+++ fish/config_abbr.fish
@@ -0,0 +1,23 @@
+# fish abbreviations
+abbr -a aba 'abbr -a'
+abbr -a abp 'abbr | peco'
+abbr -a abs 'abbr -s'
+abbr -a bri 'brew install'
+abbr -a brud 'brew update'
+abbr -a brug 'brew upgrade'
+abbr -a cat bat
+abbr -a dc docker-compose
+abbr -a dcd 'docker-compose down'
+abbr -a dcs 'docker-compose stop'
+abbr -a dcu 'docker-compose up -d'
+abbr -a gco 'git checkout'
+abbr -a gcob 'git checkout -b'
+abbr -a his history
+abbr -a hisp 'history | peco'
+abbr -a ll 'exa -abghHl'
+abbr -a ls exa
+abbr -a ps procs
+abbr -a psp 'procs | peco'
+abbr -a py python
+abbr -a sfishc 'source ~/.config/fish/config.fish'
+abbr -a vfishc 'vim ~/.config/fish/config.fish'
