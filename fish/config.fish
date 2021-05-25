2eaf14df0993dd4e40a9a3ea976e29313035c55a
p-onoshi <tomoyasu.ono@paiza.co.jp>
2021-05-25 12:52:24 +0900

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -13,8 +13,25 @@ set fish_theme agnoster
 # PATH
 set PAIZA_CHECKOUT_PATH /Users/m1071/ghq/github.com/gi-no/paiza/ $PATH
 
+# TZ
+set TZ "JST-9"
+
+fish_add_path /usr/local/opt/imagemagick@6/bin
+
+# prompt configs
+set -U theme_display_date no 
+set -U theme_display_cmd_duration yes
+set -U theme_newline_cursor no
+set -U theme_display_docker_machine yes
+set -U theme_color_scheme dracula
+
 
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
-fish_add_path /usr/local/opt/imagemagick@6/bin
+
+# peco-cd for ghq
+function cdg
+    ghq list --full-path | peco | read dist
+    cd $dist
+end
