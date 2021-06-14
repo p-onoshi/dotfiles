de21ace9862935eebc218697ed46a0b5803b4a5f
p-onoshi <tomoyasu.ono@paiza.co.jp>
2021-06-14 16:20:50 +0900

    update fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,11 @@ eval (rbenv init - | source)
 # asdf
 # source ~/.asdf/asdf.fish
 
-# theme
-set fish_theme agnoster
+# fzf
+set -U FZF_LEGACY_KEYBINDINGS 0
 
 # PATH
-set PAIZA_CHECKOUT_PATH /Users/m1071/ghq/github.com/gi-no/paiza/ $PATH
+set PAIZA_CHECKOUT_PATH /Users/m1096/ghq/github.com/gi-no/paiza/ $PATH
 
 # TZ
 set TZ "JST-9"
@@ -24,14 +24,3 @@ set -U theme_display_cmd_duration yes
 set -U theme_newline_cursor no
 set -U theme_display_docker_machine yes
 set -U theme_color_scheme dracula
-
-
-# peco-cd for ghq
-function cdg
-  cd (ghq list --full-path | peco)
-end
-
-function fish_user_key_bindings
-  bind \cr peco_select_history # Bind for peco history to Ctrl+r
-  bind \cg cdg                 # Bind for peco-cd for ghq to Ctrl+g
-end
